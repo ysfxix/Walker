c2b2d15355055b528a006379418148dcb70c8770
yusuf-alii <ysf.ali@gmail.com>
2019-12-18 12:15:37 +0530

    Added functionality to dynamically spawn (create) walkers during runtime by users

--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
-var walker1;
+// var walker1;
+var walkers = [];
 
 function setup() {
   createCanvas(600, 600);
-  walker1 = new Walker(width/2, height/2);
+  // walker1 = new Walker(width/2, height/2);
   // walker1.sayHello();
 }
 
 function draw() {
   background(100,100,100,100);
   
-  walker1.move();
-  //console.log(walker1.x + " : " + walker1.y);
-  walker1.display();  
+  // walker1.move();
+  // walker1.display();  
+
+  // fetch all the walkers from the array and display them on the canvas
+  walkers.forEach((w) => {
+    // console.log(w);
+    w.move();
+    w.display();
+  });
+}
+
+// Create a new Walker object and push it into walkers[] array
+function spawnWalker() {
+  walkers.push(new Walker(width/2, height/2));
 }
